de437c19867461b9477f9e579fbb22e7f08c19b6
ELBIT_NTor01976 <mark.mishaev@elbitsystems.com>
2018-11-13 07:06:35 +0200

    count number of inversions in array (brute force/divide and conquer)

---
+++ src/CountNumberOfInversionsTest.java
@@ -0,0 +1,42 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CountNumberOfInversionsTest {
+
+    @Test
+    public void findInversionsByBruteForce1() {
+
+        int[] array = {3, 2, 1};
+
+        List<Pair> expectedInversions = new ArrayList<>();
+        expectedInversions.add(new Pair(3, 2));
+        expectedInversions.add(new Pair(3, 1));
+        expectedInversions.add(new Pair(2, 1));
+
+        List<Pair> actualInversions = CountNumberOfInversions.findInversionsByBruteForce(array);
+
+        assertThat(actualInversions, is(expectedInversions));
+    }
+
+    @Test
+    public void findInversionsByBruteForce2() {
+
+        int[] array = {1, 3, 5, 2, 4, 6};
+
+        List<Pair> expectedInversions = new ArrayList<>();
+        expectedInversions.add(new Pair(3, 2));
+        expectedInversions.add(new Pair(5, 2));
+        expectedInversions.add(new Pair(5, 4));
+
+        List<Pair> actualInversions = CountNumberOfInversions.findInversionsByBruteForce(array);
+
+        assertThat(actualInversions, is(expectedInversions));
+    }
+}
---
+++ src/Pair.java
@@ -0,0 +1,30 @@
+public class Pair extends Object {
+    private int i;
+    private int j;
+
+    public Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return this.i;
+    }
+
+    public int getJ() {
+        return this.j;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.getI() == ((Pair) obj).getI() && this.getJ() == ((Pair) obj).getJ();
+    }
+}
